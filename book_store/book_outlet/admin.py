d07b7a04e90189b4edeaf39ef666460f2b867de8
abdur rahim <abdur6552@gmail.com>
2021-11-08 10:58:46 +0600

    relation stablished, foreign key, one to many

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book,Author
 # Register your models here.
 
 
 class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ("slug",)
     prepopulated_fields = {"slug":("title",)}
+    list_filter = ("author","rating")
+    list_display = ("title","author")
+
+
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ("first_name","last_name")
 
 admin.site.register(Book,BookAdmin)
+admin.site.register(Author)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -4,13 +4,23 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.text import slugify
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
 
+
+    def full_name(self):
+        return f"{self.first_name}  {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(8)])
-    author = models.CharField(max_length=100, null=True)
+    # author = models.CharField(max_length=100, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE,null=True,related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False,db_index=True)
 
@@ -24,3 +34,5 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
+
+

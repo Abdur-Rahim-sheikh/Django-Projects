b2813092992ab240e4b4fc8f8c463ecca87a3bd7
abdur rahim <abdur6552@gmail.com>
2021-11-11 17:24:38 +0600

    django form help used for making form

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from .forms import ReviewForm
 # Create your views here.
 
 def review(request):
     if request.method== "POST":
-        entered_username = request.POST['username']
-        if entered_username=="":
-            return render(request,"reviews/review.html",{
-                "has_error":True
-            })
-        print(entered_username)
-        return HttpResponseRedirect("/thank-you")
+        form = ReviewForm(request.POST)
 
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("/thank-you")
+    else:
+        form = ReviewForm()
+        
     return render(request,"reviews/review.html",{
-        "has_error":False
+        "form":form
     })
+
 
 
 def thank_you(request):
